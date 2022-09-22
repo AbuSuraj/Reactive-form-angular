abcd99267b1d5896f4138561a63179b1420b5ae4
abu bokor suraj <abusurajbd@gmail.com>
2022-09-22 17:07:23 +0600

    use angular in reactive forms

--- form/src/app/app.component.ts
+++ form/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {FormGroup,FormControl} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-form';
+  registrationForm = new FormGroup({
+    userName: new FormControl('Abu'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  })
 }
